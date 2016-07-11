7b9d31b8726200b4d52710ace7018879cd67d9e7
galvanizejs <js@matthewleach.com>
2016-07-11 15:08:45 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,91 @@
+/*
+=======================================================
+** Week 1 - Project 1 **
+Below are a number of problems for you to solve
+using JS. The JS code can be written below each
+problem and the result can be logged to the console.
+In order to make the results easy to read,
+please use console.log("Question #") to declare the
+problem number being solved followed by console.log(yourAnswer) after each question.
+EXAMPLE:
+// 1. Log your 'hello world' to the console.
+console.log("Question 1")
+console.log("hello world")
+BASIC TRACK: 1-7
+ADVANCED TRACK: 8-11
+=======================================================
+*/
+
+
+
+// 1. Log your name to the console (as a string).
+console.log("Question 1");
+
+
+
+
+// 2. Declare a new variable called 'name',
+//    and then log it to the console.
+
+
+
+
+
+// 3. Declare two variables with number values.
+//    Add, subtract, multiply and divide them.
+
+
+
+
+
+// 4. Declare two variables with string values.
+//    Create a third variable that concatenates the two strings
+//    that also places a space between the strings.
+
+
+
+
+
+// 5. Add, subract, multiply and divide the strings '5' and '3'.
+//    Observe the results and brieftly explain the results within the console.
+
+
+
+
+
+// 6. Create an simple alert stating anything you wish. 
+
+
+
+
+
+// 7. Create a prompt that asks a user to pick a number.
+//    Then double user's answer.
+
+
+
+
+// ADVANCED TRACK
+
+// 8. Ask for a user's name, age and city.
+//    In a single command display/log the sentence:
+//   "My name is __. I'm __ years old, and I live in __."
+
+
+
+
+
+// 9. Create an alert that displays the response from a prompt. You can prompt the user for any kind of fun response. 
+
+
+
+
+
+// 10. Create the formula to determine the area of a rectangle.
+//     The rectangle is: 2 inches by 3 inches
+//     Display the result in the console. 
+
+
+
+
+// 11. Prompt a user for their name. Find the length (number of characters) of their name and display the result in the console. 
