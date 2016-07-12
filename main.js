6ffea816d0671b01aef621697c9060b878a414db
galvanizejs <js@matthewleach.com>
2016-07-12 14:57:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -69,9 +69,29 @@ console.log("Question 1");
 
 
 
+
+// 8. Set the variable 'num1' to 5.
+//    Set the variable 'num2' to 10.
+//    Test to see if num1 is greater than num2. 
+//    If so, output to the console: Yes, the value of num1 is greater than num2
+//    If not, output to the console: No, the value of num1 is not greater than num2
+
+
+
+
+
+
+// 9. Set the variable 'score' to a number between 1-100. 
+//    Create the following conditions:
+//    If the score is above 90, output to the console "great job".
+//    If the score is between 60 and 89, output to the console "nice work, keep practicing".
+//    If the score is below 59, output to the console "very bad: study more and retake the test".
+
+
+
 // ADVANCED TRACK
 
-// 8. Ask for a user's name, age and city using a prompt.
+// A. Ask for a user's name, age and city using a prompt.
 //    In a single command, take the user's answers and log the sentence:
 //   "My name is __. I'm __ years old, and I live in __."
 
@@ -79,29 +99,33 @@ console.log("Question 1");
 
 
 
-// 9. Create an alert that displays the response from a prompt. You can prompt the user for any kind of fun response. 
+// B. Create an alert that displays the response from a prompt. You can prompt the user for any kind of fun response. 
 
 
 
 
 
-// 10. Create the formula to determine the area of a rectangle.
+// C. Create the formula to determine the area of a rectangle.
 //     The rectangle is: 2 inches by 3 inches
 //     Display the result in the console. 
 
 
 
-//11. Prompt users to input the length and width of a rectangle and log the result.
+// D. Prompt users to input the length and width of a rectangle and log the result.
 
 
 
 
 
-// 12. Prompt a user for their name. Find the length (number of characters) of their name and display the result in the console. 
+// E. Prompt a user for their name. Find the length (number of characters) of their name and display the result in the console. 
 
 
 
 
+// F. Set a prompt to request a test between a score of 0-100 from the user. 
+//    If the score is above 90, set an alert to tell the user "great job".
+//    If the score is between 60 and 89, set an alert to tell the user "nice work, keep practicing".
+//    If the score is below 59, set an alert to tell the user "very bad: study more and retake the test".
 
 
 
