36285a73141a4e11459604ddb0cce15a78f46c04
galvanizejs <js@matthewleach.com>
2016-08-17 12:11:14 -0600

    Solutions should now present in the HTML page.

--- main.js
+++ main.js
@@ -4,137 +4,140 @@
 
 Below are a number of problems for you to solve
 using JS. The JS code can be written below each
-problem and the result can be logged to the console.
-In order to make the results easy to read,
-please use console.log("Question #") to declare the
-problem number being solved followed by console.log(yourAnswer) after each question.
-
-EXAMPLE:
-// 1. Log your 'hello world' to the console.
-console.log("Question 1")
-console.log("hello world")
+problem.
 
 =======================================================
 */
 
 
-
-// 1. Log your name to the console (as a string).
-console.log("Question 1");
-
+// 1. Connect the main.js document to the HTML page.
+//    Then Update the Text to Green by adding the .status-good class.
 
 
 
-// 2. Declare a new variable called 'name',
-//    store your name within it
-//    and then log it to the console.
+// When the JS is connected properly, the following code will update the status message. 
+document.getElementById("q1").innerHTML = "JS Page Connected Properly!";
+document.getElementById("q1").classList.add("status-good");
+
+
+
+// 2. Declare a new variable called 'feelings', 
+//    Then store a string with a word that describes   
+//    how well you feel about this course material. 
+//    Then display that string within the HTML page. 
 
 
 
 
 
-// 3. Declare two variables with number values.
+
+
+// 3. Declare a new variable called 'name',
+//    store your name within it, and then
+//    add it with the 'message' variable, and then 
+//    display it within the HTML page.
+
+
+
+
+
+
+
+// 4. Declare two variables with number values.
 //    Add, subtract, multiply and divide them.
 //    Display the values of each within the console.
+//    Then label these values the best you can. 
 
 
 
 
 
-// 4. Declare two variables with string values.
+
+
+// 5. Declare two variables with string values.
 //    Create a third variable that concatenates the two strings
-//    that also places a space between the strings.
+//    that also places the appropriate space between the strings.
 //    Log the value of the third variable to the console.
 
 
 
 
-// 5. Add, subract, multiply and divide the strings '5' and '3'.
-//    Observe the results and brieftly explain the results within the console.
 
 
 
-
-
-// 6. Create an simple alert stating anything you wish. 
-
-
-
-
-
-// 7. Create a prompt that asks a user to pick a number.
-//    Then double user's answer and log that into the console.
+// 6. Add, subract, multiply and divide the strings '5' and '3'.
+//    Observe the results and brieftly explain the results within the HTML page.
 
 
 
 
 
 
-// 8. Set the variable 'num1' to 5.
+
+
+// 7. When a button is clicked, add content into the ID "q7". 
+
+
+
+
+
+
+
+// 8. Collect a number value from the user and when a button is clicked, 
+//    then double user's answer and display that value into the HTML page.
+
+
+
+
+
+
+
+// 9. Set the variable 'num1' to 5.
 //    Set the variable 'num2' to 10.
 //    Test to see if num1 is greater than num2. 
-//    If so, output to the console: Yes, the value of num1 is greater than num2
-//    If not, output to the console: No, the value of num1 is not greater than num2
+//    If so, output to the HTML page: Yes, the value of num1 is greater than num2
+//    If not, output to the HTML page: No, the value of num1 is not greater than num2
 
 
 
 
 
 
-// 9. Set the variable 'score' to a number between 1-100. 
+
+// 10. Set the variable 'score' to a number between 1-100. 
 //    Create the following conditions:
-//    If the score is above 90, output to the console "great job".
-//    If the score is between 60 and 89, output to the console "nice work, keep practicing".
-//    If the score is below 59, output to the console "very bad: study more and retake the test".
+//    If the score is above 90, output to the HTML page "Great job".
+//    If the score is between 60 and 89, output to the HTML page "Nice work, keep practicing".
+//    If the score is below 59, output to the HTML page "Very bad: study more and retake the test".
+//    Test to make sure each condition is met properly. 
+
+
+
+
 
 
 
 // ADVANCED TRACK
 
-// A. Ask for a user's name, age and city using a prompt.
-//    In a single command, take the user's answers and log the sentence:
+// A. Ask for a user's name, age and city using input fields.
+//    In a single command, take the user's answers and display into the HTML page the sentence:
 //   "My name is __. I'm __ years old, and I live in __."
 
 
 
 
 
-// B. Create an alert that displays the response from a prompt. You can prompt the user for any kind of fun response. 
 
-
-
-
-
-// C. Create the formula to determine the area of a rectangle.
+// B. Create the formula to determine the area of a rectangle.
 //     The rectangle is: 2 inches by 3 inches
-//     Display the result in the console. 
-
-
-
-// D. Prompt users to input the length and width of a rectangle and log the result.
+//     Display the result in the HTML page. 
 
 
 
 
 
-// E. Prompt a user for their name. Find the length (number of characters) of their name and display the result in the console. 
 
-
-
-
-// F. Set a prompt to request a test between a score of 0-100 from the user. 
-//    If the score is above 90, set an alert to tell the user "great job".
-//    If the score is between 60 and 89, set an alert to tell the user "nice work, keep practicing".
-//    If the score is below 59, set an alert to tell the user "very bad: study more and retake the test".
-
-
-
-
-// SANDBOX TRACK
-
-// Solving all of these problems is a great step in the right direction, but the next step is coming up 
-// with your own arrangements to solve new problems. Create your own stories to solve and find the solution. 
-// Feel free to share with your classmates to challenge them! 
+// C. Collect the use's length and width input to determine the area of a rectangle and display within the HTML page the result.
 
 
 
@@ -142,4 +145,31 @@ console.log("Question 1");
 
 
 
-// Congrats! You are complete!
+// D. Have the user enter their name into a textfield. When they click a button, find the length (number of characters) of their name and display the result in the HTML page. 
+
+
+
+
+
+
+
+// E. Create a textfield to request a test score between 0-100 from the user. 
+//    If the score is above 90, display in the HTML a message to tell the user "Great job!".
+//    If the score is between 60 and 89, display a message of "Nice work, keep practicing.".
+//    If the score is below 59, display a message of "Very bad: study more and retake the test.".
+//    You can get fancy by adding CSS styles to each of these displays to change the color, too - Green=Good, Orange=OK, Red=Bad
+
+
+
+
+
+/* SANDBOX TRACK
+
+Solving all of these problems is a great step in the right direction, 
+but the next step is coming up with your own arrangements to solve 
+new problems. Practice creating your own problems to solve and their solutions -
+you can even challenge your classmates! 
+ 
+Also, consider how you can add/remove CSS styles to create added presentation with the results. 
+
+*/
